1cf6749838eb87bdae71bbfa977e87138f7c2698
霜越 正伸 <shimokoshimasanobu@MacBook-Mash.local>
2018-05-20 13:50:04 +0900

    dynamic routingを設定した

--- Bottle Practice/Bottle Template Study/run.py
+++ Bottle Practice/Bottle Template Study/run.py
@@ -10,5 +10,12 @@ def hello():
 def test_route():
     return "<B>TEST!</B>"
 
+#ダイナミックルーティング。URLに変数を仕込める
+@route('/VAL/') #デフォルトの場合
+@route('/VAL/<comment>')   #変数名は comment である
+def view_val(comment='デフォルト'): #デフォルトの引数に 'デフォルト' を設定
+    return '<I>{comment}</I>'.format(comment=comment)
 
+
+#WEBサーバが立ち上がる。ブラウザからURLが入力されたらそれに対する処理を行う
 run(host='localhost', port=8080, debug=True)
