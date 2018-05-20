229db209503ea48533c8f2c4daa7c907d8942b44
霜越 正伸 <shimokoshimasanobu@MacBook-Mash.local>
2018-05-20 17:42:03 +0900

    メール送信機能の実装を始める（未完成）

--- Bottle Practice/Bottle_Practice/index.py
+++ Bottle Practice/Bottle_Practice/index.py
@@ -2,6 +2,11 @@
 import os
 from bottle import run,route,template,redirect,request,post
 from bottle import TEMPLATE_PATH, jinja2_template as template
+# Import smtplib for the actual sending function
+import smtplib
+
+# Import the email modules we'll need
+from email.mime.text import MIMEText
  
 # index.pyが設置されているディレクトリの絶対パスを取得
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
@@ -22,6 +27,31 @@ def post_val():
     msg_val = request.forms.msg_val
     ret_text = template("form_out",name_val=name_val,mail_val=mail_val,msg_val=msg_val)
     return ret_text
+
+@route('/send_mail',method=["POST"])
+def send_mail():
+    result = ""
+    # メール送信モジュールを使ってメールを送る
+    #name_val = request.forms.name_val
+    mail_val = request.forms.mail_val
+    msg_val = request.forms.msg_val
+
+    from_address  = 'mash@star.ocn.ne.jp'
+    to_address    = mail_val
+    msg = MIMEText(msg_val)
+    
+    msg['Subject'] = 'Mail sending test.'
+    msg['From']    = from_address
+    msg['To']      = to_address    
+
+    #ホストの設定とかが必要になる
+    # Send the message via our own SMTP server, but don't include the envelope header.
+    s = smtplib.SMTP()
+    s.connect()
+    s.sendmail( from_address, to_address, msg.as_string() )
+    s.close()
+
+    return result
 
 
 if __name__ == "__main__":
--- Bottle Practice/Bottle_Practice/views/form_out.tpl
+++ Bottle Practice/Bottle_Practice/views/form_out.tpl
@@ -9,6 +9,17 @@
         name : {{name_val}}<br/>
         mail : {{mail_val}}<br/>
         msg : {{msg_val}}<br/>
+        <form action="/send_mail" method="post">
+            <input type="hidden" id="name" name="name_val" value="{{name_val}}"/>
+            <input type="hidden" id="mail" name="mail_val" value="{{mail_val}}"/>
+            <input type="hidden" id="msg" name="msg_val" value="{{msg_val}}"/>
+            <input type="submit" value="next"/>
+        </form>
+        <form action="/top">
+            <!-- 単に戻るだけのボタンなら、POSTの定義は不要。form の action だけ設定されていれば十分 -->
+            <input type="submit" value="return"/>
+        </form>
+        
     </body>
 
 </html>
