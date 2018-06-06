1172b13fa6d68002d175815a4b4086a072e3fd6b
M Shimokoshi <mash1975@msn.com>
2018-06-06 17:35:42 +0900

    作成開始

--- Bottle Practice/TwitterLog_Getter/Get_TwitterLog.py
+++ Bottle Practice/TwitterLog_Getter/Get_TwitterLog.py
@@ -1,4 +1,14 @@
 # -*- coding: utf-8 -*-
 import os
 import json
+import OAuth_ORG
+from requests_oauthlib import OAuth1Session
 
+#JSON によりログを取得する
+def GetTwitterLog(stat):
+    CK = OAuth_ORG.CK
+    CS = OAuth_ORG.CS
+    AT = OAuth_ORG.AT
+    AS = OAuth_ORG.AS
+    twitter = OAuth1Session(CK, CS, AT, AS)
+    return twitter
